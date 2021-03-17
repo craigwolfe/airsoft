3aa0ed3cfa330abb7cc5fa55fe2a5341a91daa85
Craig Wolfe <craig.s.wolfe@me.com>
2021-03-17 08:53:11 -0400

    removed backgroundcolor

--- airsoftapp/components/App.js
+++ airsoftapp/components/App.js
@@ -1,6 +1,6 @@
 //import liraries
-import React, { Component } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
 
 // create a component
 const App = () => {
@@ -17,7 +17,6 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#2c3e50',
   },
 });
 
