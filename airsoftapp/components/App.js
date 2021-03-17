8999212d84c991eb4a6348de17b005f6d39e5638
Craig Wolfe <craig.s.wolfe@me.com>
2021-03-17 10:50:33 -0400

    add class App

--- airsoftapp/components/App.js
+++ airsoftapp/components/App.js
@@ -1,17 +1,19 @@
 //import liraries
-import React from 'react';
+import React, {Component} from 'react';
 import {View, Text, StyleSheet} from 'react-native';
 
 // create a component
-const App = () => {
-  return (
-    <View style={styles.container}>
-      <Text>
-        Welcome to Plink. The mobile app for Airsoft Players and field owners.
-      </Text>
-    </View>
-  );
-};
+class App extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>
+          Welcome to Plink. The mobile app for Airsoft Players and field owners.
+        </Text>
+      </View>
+    );
+  }
+}
 
 // define your styles
 const styles = StyleSheet.create({
