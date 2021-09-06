76fcf9b633676543ac3ece001e63a0dcc685cb9b
Craig Wolfe <craig.s.wolfe@me.com>
2021-09-06 19:52:11 -0400

    Update App.js
    Chgd fire base variable

--- airsoftapp/components/App.js
+++ airsoftapp/components/App.js
@@ -16,7 +16,7 @@ class App extends Component {
   componentDidMount() {
     // web app's Firebase configuration
     // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-    var firebaseConfig = {
+    const firebaseConfig = {
       
       
     };
